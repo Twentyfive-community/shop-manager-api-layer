cf45b1da60ff8d03e8aaa70689541903e487766c
Raikozu <francescogaglianese93@gmail.com>
2025-01-11 18:58:31 +0100

    -Keycloak authentication added. We should protect our mappings with correct roles.

--- src/main/java/org/twentyfive/shop_manager_api_layer/ShopManagerApplication.java
+++ src/main/java/org/twentyfive/shop_manager_api_layer/ShopManagerApplication.java
@@ -2,8 +2,10 @@ package org.twentyfive.shop_manager_api_layer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.twentyfive.shop_manager_api_layer", "com.twentyfive.authorizationflow"})
 public class ShopManagerApplication {
 
     public static void main(String[] args) {
