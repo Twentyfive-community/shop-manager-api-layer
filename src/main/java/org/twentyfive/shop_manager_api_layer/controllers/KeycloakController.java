a8cfa474f7f45b4dda4c0875dd3b1330001699c9
Raikozu <francescogaglianese93@gmail.com>
2025-01-25 14:02:48 +0100

    -Action created.

--- src/main/java/org/twentyfive/shop_manager_api_layer/controllers/KeycloakController.java
+++ src/main/java/org/twentyfive/shop_manager_api_layer/controllers/KeycloakController.java
@@ -17,4 +17,9 @@ public class KeycloakController {
     public String login(@RequestBody TokenRequest tokenRequest) {
         return keycloakService.getAccessToken(tokenRequest.getClient_id(), tokenRequest.getClient_secret(), tokenRequest.getUsername(), tokenRequest.getPassword());
     }
+
+    @PatchMapping("/reset-password")
+    public ResponseEntity<String> resetPassword(@RequestBody TokenRequest tokenRequest) {
+        return null; //TODO
+    }
 }
