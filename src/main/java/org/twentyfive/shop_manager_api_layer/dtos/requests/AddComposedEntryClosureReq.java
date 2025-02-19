7720274a09a56e9179c22ec3054a5eb8245bf93e
Raikozu <francescogaglianese93@gmail.com>
2025-02-19 17:30:34 +0100

    If we insert composedEntries with first letter lowercase we fix it with uppercase

--- src/main/java/org/twentyfive/shop_manager_api_layer/dtos/requests/AddComposedEntryClosureReq.java
+++ src/main/java/org/twentyfive/shop_manager_api_layer/dtos/requests/AddComposedEntryClosureReq.java
@@ -13,4 +13,16 @@ import java.util.List;
 public class AddComposedEntryClosureReq {
     private String composedLabelEntry;
     private List<LabelAndValue> labelAndValues;
+
+    public List<LabelAndValue> getLabelAndValues() {
+        if (labelAndValues != null) {
+            labelAndValues.forEach(entry -> {
+                String label = entry.getLabel();
+                if (label != null && !label.isEmpty()) {
+                    entry.setLabel(Character.toUpperCase(label.charAt(0)) + label.substring(1));
+                }
+            });
+        }
+        return labelAndValues;
+    }
 }
